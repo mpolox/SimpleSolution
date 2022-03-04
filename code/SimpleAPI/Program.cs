d431df2570d87f7be7fc32dc3815b6992b06f2ae
Marcopolo Ramos <mpolox@gmail.com>
2022-03-03 22:32:20 -0600

    update swagger

--- code/SimpleAPI/Program.cs
+++ code/SimpleAPI/Program.cs
@@ -10,16 +10,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+// app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    // app.UseSwaggerUI();
-    app.UseSwaggerUI(c =>
-    {
-        c.RoutePrefix = "";
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-    });
-}
+    c.RoutePrefix = "";
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
 
 app.UseHttpsRedirection();
 
