28bef8b254e0597b685f6375af7457d36b881c3c
Marcopolo Ramos <mpolox@gmail.com>
2022-03-03 22:24:45 -0600

    update swagger

--- code/SimpleAPI/Program.cs
+++ code/SimpleAPI/Program.cs
@@ -13,7 +13,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    // app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.RoutePrefix = "";
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
