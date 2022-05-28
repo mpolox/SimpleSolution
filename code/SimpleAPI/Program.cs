95457d112537eb12395109e4555b3bf4cc707f3f
Marcopolo Ramos <mpolox@gmail.com>
2022-05-27 23:47:17 -0500

    update test server

--- code/SimpleAPI/Program.cs
+++ code/SimpleAPI/Program.cs
@@ -1,9 +1,26 @@
 using SimpleAPI.Data;
 using SimpleAPI.Data.interfaces;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder
+                          .WithOrigins("http://localhost:3000", "https://localhost", "https://agreeable-forest-022536b0f.1.azurestaticapps.net/");
+                          //.AllowAnyOrigin();
+                          //.AllowAnyMethod()
+                          //.AllowAnyHeader()
+                          //.AllowCredentials();
+                      });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,6 +38,8 @@ app.UseSwaggerUI(c =>
     c.RoutePrefix = "";
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
 });
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
